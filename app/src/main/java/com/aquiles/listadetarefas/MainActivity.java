39d73e0f5c41b8f21eeb386e2bb821747a173d41
Iarlen Aquiles <iarlensilva775@gmail.com>
2021-10-25 19:54:22 -0300

    initial commit

---
+++ app/src/main/java/com/aquiles/listadetarefas/MainActivity.java
@@ -0,0 +1,46 @@
+package com.aquiles.listadetarefas;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.aquiles.listadetarefas.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AdicionarTarefaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+}
